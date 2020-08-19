e0221a02cc316bdb550c84dc96867f03d5a39ce3
Greg Fielding <greg@fieldingdesigngroup.com>
2020-08-18 21:58:32 -0700

    distrubution update

--- src/views/Partners/Distribution.vue
+++ src/views/Partners/Distribution.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="page distribution">
-    Distribution Partners
+
+  	<div class="distribution__hero">
+  		<div class="container flex align-end">
+  			<div class="distribution__hero-text mb-5">
+	  			<h1>Distribution Partners</h1>
+	  			<h3>owubgu0 w08g0 8gw 08wg f0gw 08w-8g -w</h3>
+  			</div>
+  		</div>
+  	</div>
+  	<div class="distribution__map">
+  		<div class="distribution__map-col-1">
+  			<iframe src="https://snazzymaps.com/embed/255724" width="100%" height="700px" style="border:none;"></iframe>
+  		</div>
+  		<div class="distribution__map-col-2">
+  			
+  		</div>
+  	</div>
+    
   </div>
 </template>
 
